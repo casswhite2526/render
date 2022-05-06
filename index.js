5c2ea4ad49e85ce78b19e240372d7bd2dd17e5b3
makabe <fghigg3250.ju@gmail.com>
2022-05-06 20:31:25 +0900

    make it better

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 import axios from 'axios'
 const baseUrl = process.env.LAMBDA_INVOKE_PATH
 
+var serverstatus
+
 client.once("ready", async () => {
   console.log("Ready!");
 })
@@ -15,6 +17,7 @@ client.on('messageCreate', message => {
       .then(res => {
         message.reply(`サーバーを起動しました。`)
 		message.reply(res.data)
+		serverstatus = 'サーバー稼働中'
       })
       .catch(err => {
 		if (err.response.status == 400) {
@@ -33,11 +36,15 @@ if (message.content === '!mcstop') {
   axios.get(`${baseUrl}ec2-down`)
     .then(res => {
       message.reply('サーバーを停止しました。')
+	  serverstatus = 'サーバー停止中'
   })
   .catch(err　=> {
     message.reply('既に停止しています。')
   })
 }
+
+if (message.content === '!mcstatus') {
+	message.reply(serverstatus)
 
 })
 
