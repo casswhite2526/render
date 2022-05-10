26639b4e707d9ef7b4f952830fcdbd16a152c5f4
makabe <fghigg3250.ju@gmail.com>
2022-05-11 05:27:54 +0900

    make it better

--- index.js
+++ index.js
@@ -46,8 +46,12 @@ if (message.content === '!mcstop') {
 if (message.content === '!mcstatus') {
 	axios.get('${baseUrl}ec2-status')
 	.then(res => {
-		message.reply('mcstatus')
-})
+		message.reply('hi')
+	})
+	.catch(err => {
+		message.reply('error')
+	})	
+
 }
 })
 
